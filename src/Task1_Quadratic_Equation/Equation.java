df19d27c722e76efc6d070f4c865a51de7765a16
AnechAb <abubekirova@inbox.ru>
2019-12-05 19:11:02 +0300

    small changes

--- src/Task1_Quadratic_Equation/Equation.java
+++ src/Task1_Quadratic_Equation/Equation.java
@@ -4,32 +4,29 @@ import java.util.Scanner;
 
 public class Equation {
     public static void main(String[] args) {
-        eq();
+        resolve();
     }
 
-    public static void eq() {
-        double a, b, c, D;
-        System.out.println("Enter a, b and c separated by space: ");
+    public static void resolve() {
+        double a = 1;
+        double b = 4;
+        double c = 4;
+        double descriminant;
 
-        Scanner in = new Scanner(System.in);
-        a = in.nextDouble();
-        b = in.nextDouble();
-        c = in.nextDouble();
-
-        D = b * b - 4 * a * c;
-        if (D > 0) {
+        descriminant = b * b - 4 * a * c;
+        if (descriminant > 0) {
             double x1, x2;
-            x1 = (-b - Math.sqrt(D)) / (2 * a);
-            x2 = (-b + Math.sqrt(D)) / (2 * a);
+            x1 = (-b - Math.sqrt(descriminant)) / (2 * a);
+            x2 = (-b + Math.sqrt(descriminant)) / (2 * a);
             System.out.println("x1 = " + x1 + ", x2 = " + x2);
         }
-        else if (D == 0) {
+        else if (descriminant == 0) {
             double x;
             x = -b / (2 * a);
             System.out.println("x = " + x);
         }
         else {
-            System.out.println("Discriminant is negative. Try with another values.");
+            System.out.println("No solutions.");
         }
     }
 }
---
+++ src/Task1_Quadratic_Equation_with_OOP_basics/Quadratic_Equation.java
@@ -0,0 +1,12 @@
+package Task1_Quadratic_Equation_with_OOP_basics;
+
+public class Quadratic_Equation {
+
+    private static void resolveEquation(float a, float b, float c){
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
